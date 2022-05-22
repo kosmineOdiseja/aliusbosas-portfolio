1c83395d32fbc4ba5245eeafbe5a593e708d14cf
Alius <aliusbosas@gmail.com>
2022-05-22 11:41:01 +0300

    changed font style and working on hero section

--- src/components/heroSection/Hero.tsx
+++ src/components/heroSection/Hero.tsx
@@ -4,15 +4,20 @@ import { StaticImage } from "gatsby-plugin-image"
 const Hero = () => {
 
   return (
-	  <div className='bg-indigo-500' >
-	  	<section className='flex justify-items-center align-center h-screen lg'>
-			<div>
-				<h3> Sveiki, aš esu   </h3>
-				<h1> Alius Bosas</h1>
-				<div> Aš ieškau front-end darbo </div> 
+	  <div className='bg-indigo-900 h-screen flex justify-center' >
+	  	<section className='container lg w-full	h-auto flex flex-row items-center justify-start '>
+			<div className='flex justify-center container lg items-start gap-5 flex-col mx-0 my-auto'>
+				<h1 className='text-lg text-green-400 '> Sveiki, aš esu </h1>
+				<h2 className='text-5xl text-yellow-500 font-semibold '> Alius Bosas </h2>
+				<code className='text-3xl text-green-600'> Aš noriu užaugti su Jumis </code> 
 			</div>
-
-		  </section>
+		  <StaticImage
+		  	src='../../assets/images/programmer-hero.png'
+			className='flex-1 '
+			alt='programmer'
+			placeholder="blurred"
+			/>
+		</section>
 	  </div>
   )
 }
