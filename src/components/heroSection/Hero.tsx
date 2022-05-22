532e3b3148a8e1b1ebbe93e4e1bd64027ccf3382
Alius <aliusbosas@gmail.com>
2022-05-22 14:16:00 +0300

    hero section

--- src/components/heroSection/Hero.tsx
+++ src/components/heroSection/Hero.tsx
@@ -4,9 +4,10 @@ import { StaticImage } from "gatsby-plugin-image"
 const Hero = () => {
 
   return (
-	  <div className='bg-indigo-900 h-screen flex justify-center' >
-	  	<section className='container lg w-full	h-auto flex flex-row items-center justify-start '>
-			<div className='flex justify-center container lg items-start gap-5 flex-col mx-0 my-auto'>
+
+	  <div className='bg-indigo-900 h-screen items-center flex  justify-center' >
+	  	<section className='container flex flex-row gap-8 w-auto h-auto'>
+			<div className='flex justify-center lg items-start gap-5 flex-col '>
 				<h1 className='text-lg text-green-400 '> Sveiki, aš esu </h1>
 				<h2 className='text-5xl text-yellow-500 font-semibold '> Alius Bosas </h2>
 				<code className='text-3xl text-green-600'> Aš noriu užaugti su Jumis </code> 
@@ -16,6 +17,8 @@ const Hero = () => {
 			className='flex-1 '
 			alt='programmer'
 			placeholder="blurred"
+			width={500}
+			height={500}
 			/>
 		</section>
 	  </div>
