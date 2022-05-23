e8480085ba1da02a907b7fe5e537dc869ee21d62
Alius <aliusbosas@gmail.com>
2022-05-23 22:15:13 +0300

    some styles and bug fixed

--- src/components/heroSection/Hero.tsx
+++ src/components/heroSection/Hero.tsx
@@ -5,17 +5,19 @@ const Hero = () => {
 
   return (
 
-	  <div className='bg-indigo-900 h-screen items-center flex  justify-center' >
-	  	<section className='container flex flex-row gap-8 w-auto h-auto'>
+	  <div className='bg-indigo-900 h-screen items-center flex justify-center' >
+	  	<section className='container flex lg:flex-row gap-8 w-auto h-auto sm:flex-col sm:p-2 sm:gap-1 '>
 			<div className='flex justify-center lg items-start gap-5 flex-col '>
-				<h1 className='text-lg text-green-400 xl:text-lg md:text-xl '> Sveiki, aš esu </h1>
-				<h2 className='text-5xl text-yellow-300 font-semibold xl:text-4xl md:text-xl'> Alius Bosas </h2>
-				<code className='text-3xl text-yellow-400 xl:text-2xl lg:text-md md:text-xl '> Aš noriu padėti Jums augti </code> 
-				<h3 className='text-9xl text-green-300 font-bold xl:text-3xl lg:text-3xl md:text-xl sm:text-lg'> Front-end programuotojo pozicijoje! </h3>
+				<h1 className='text-lg text-green-400 xl:text-lg md:text-xl sm:text-sm '> Sveiki, aš esu </h1>
+				<h2 className='text-5xl text-yellow-300 font-semibold xl:text-4xl md:text-xl sm:text-sm'> Alius Bosas </h2>
+				<code className='text-3xl text-yellow-400 xl:text-2xl lg:text-md md:text-xl sm:text-sm sm:text-yellow-50'> Aš noriu padėti Jums augti </code> 
+				<h3 className='text-9xl text-green-300 font-bold xl:text-3xl lg:text-3xl md:text-xl sm:text-sm'> Front-end programuotojo pozicijoje! </h3>
 			</div>
 		  <StaticImage
+		  	// src='./images/svg/programmer-hero.png'
+		// idk why it doesn't work with gatsby-image from gatsby config file
 		  	src='../../assets/images/programmer-hero.png'
-			className='flex-1 '
+			className='hidden lg:block'
 			alt='programmer'
 			placeholder="blurred"
 			width={400}
--- src/components/shared/sideBar/SideBar.tsx
+++ src/components/shared/sideBar/SideBar.tsx
@@ -8,17 +8,17 @@ const SideBar = () => {
 
   return (
 	<nav className='sidebar fixed top-0 bottom-0 lg:left-0 p-2 bg-transparent flex flex-col justify-center items-start gap-5 w-10 '>
-		<a href='https://twitter.com/AliusBosas/' target='_blanck' className=''>
-			<AiFillTwitterSquare className='fill-white w-8 h-9 mx-6'/>
+		<a href='https://twitter.com/AliusBosas/' target='twitterWindow' className=''>
+			<AiFillTwitterSquare className='fill-white w-8 h-9 mx-6 hover:w-10 hover:h-11 hover:fill-yellow-500 '/>
 		</a>
-		<a href='https://github.com/kosmineOdiseja/' target='_blanck' className=''>
-			<AiFillGithub className='fill-white w-8 h-9 mx-6'/>
+		<a href='https://github.com/kosmineOdiseja/' target='githubWindow' className=''>
+			<AiFillGithub className='fill-white w-8 h-9 mx-6 hover:w-10 hover:h-11 hover:fill-yellow-500'/>
 		</a>
-		<a href='https://www.facebook.com/aliusbosas/' target='_blanck' className=''>
-			<AiFillFacebook className='fill-white w-8 h-9 mx-6'/>
+		<a href='https://www.facebook.com/aliusbosas/' target='facebookWindow' className=''>
+			<AiFillFacebook className='fill-white w-8 h-9 mx-6 hover:w-10 hover:h-11 hover:fill-yellow-500 '/>
 		</a>
-		<a href='https://www.linkedin.com/in/alius-bosas-6781b5189/' target='_blanck' className=''>
-			<AiFillLinkedin className='fill-white w-8 h-9 p mx-6' />
+		<a href='https://www.linkedin.com/in/alius-bosas-6781b5189/' target='linkedinWindow' className=''>
+			<AiFillLinkedin className='fill-white w-8 h-9 p mx-6 hover:w-10 hover:h-11 hover:fill-yellow-500' />
 		</a>
 	</nav>
   )
