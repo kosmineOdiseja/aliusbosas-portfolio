17d57184475e7de64f85f46b9fba05cbd4677b6a
Alius <aliusbosas@gmail.com>
2022-05-24 12:21:19 +0300

    SideBar data

--- src/components/shared/sideBar/SideBar.tsx
+++ src/components/shared/sideBar/SideBar.tsx
@@ -1,25 +1,22 @@
 import React from 'react'
-import { AiFillGithub } from 'react-icons/ai';
-import { AiFillFacebook } from 'react-icons/ai';
-import { AiFillTwitterSquare } from 'react-icons/ai';
-import { AiFillLinkedin } from 'react-icons/ai';
+import socialLinks  from '../../../data/socialLinksData.js'
+
+interface socialLinks {
+		name: string,
+		link: string, 
+		target: string,
+		icon: any,
+}
 
 const SideBar = () => {
 
   return (
 	<nav className='sidebar fixed top-0 bottom-0 lg:left-0 p-2 bg-transparent flex flex-col justify-center items-start gap-5 w-10 '>
-		<a href='https://twitter.com/AliusBosas/' target='twitterWindow' className=''>
-			<AiFillTwitterSquare className='fill-white w-8 h-9 mx-6 hover:w-10 hover:h-11 hover:fill-yellow-500 '/>
-		</a>
-		<a href='https://github.com/kosmineOdiseja/' target='githubWindow' className=''>
-			<AiFillGithub className='fill-white w-8 h-9 mx-6 hover:w-10 hover:h-11 hover:fill-yellow-500'/>
-		</a>
-		<a href='https://www.facebook.com/aliusbosas/' target='facebookWindow' className=''>
-			<AiFillFacebook className='fill-white w-8 h-9 mx-6 hover:w-10 hover:h-11 hover:fill-yellow-500 '/>
-		</a>
-		<a href='https://www.linkedin.com/in/alius-bosas-6781b5189/' target='linkedinWindow' className=''>
-			<AiFillLinkedin className='fill-white w-8 h-9 p mx-6 hover:w-10 hover:h-11 hover:fill-yellow-500' />
-		</a>
+		{socialLinks.map(({  name, icon, link, target  } : socialLinks ) => (
+				<a key={ name } href={ link } target={target} className=''>
+					{icon}
+				</a>
+			))}
 	</nav>
   )
 }
