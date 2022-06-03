32db60efb9f62a07fa2c82a0edf3ca72f8793a3a
Alius <aliusbosas@gmail.com>
2022-06-03 20:37:40 +0300

    some changes in gatsby-config file

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -12,11 +12,11 @@ const config: GatsbyConfig = {
     {
     resolve: 'gatsby-plugin-manifest',
     options: {
-        name: "GatsbyJS",
-        short_name: "GatsbyJS",
+        name: "Alius Bosas Portfolio",
+        short_name: "AliusB",
         start_url: "/",
-        background_color: "#6b37bf",
-        theme_color: "#6b37bf",
+        background_color: "#0A192F",
+        theme_color: "#0A192F",
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: "standalone",
