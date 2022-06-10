0e23f6b5cd02c155227d874c2f92eecab88baf76
Alius <aliusbosas@gmail.com>
2022-06-10 16:52:54 +0300

    small change

--- src/components/heroSection/Hero.tsx
+++ src/components/heroSection/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
 				<h2 className='text-light-text font-semibold text-4xl tablet:text-5xl tablet:font-bold tablet:tracking-wide' > Alius Bosas. </h2>
 				<code className='text-darker-text font-semibold text-2xl tablet:text-4xl'> Aš noriu Jums padėti augti</code> 
 				<h3 className='text-green font-semibold text-4xl tracking-wide tablet:text-5xl '> Front-end programuotojo pozicijoje! </h3>
-				<div className='text-light-text tracking-wide tablet:tracking-wider tablet:text-xl'>Mano tikslas - Full-Stack engineer. Šiuo metu koncentruojuosi ties Front-End technologijomis: <a href="https://reactjs.org/" className='text-green' target='ReactWindow' > React biblioteka </a>, <a href="https://www.gatsbyjs.com/" className='text-green' target='GatsbyWindow'>Gatsby karkasu.</a> Vėliau prijungsiu Back-End technologijas.</div>
+				<div className='text-light-text tracking-wide tablet:tracking-wider tablet:text-xl'>Mano tikslas - Full-Stack developer. Šiuo metu koncentruojuosi ties Front-End technologijomis: <a href="https://reactjs.org/" className='text-green' target='ReactWindow' > React biblioteka </a>, <a href="https://www.gatsbyjs.com/" className='text-green' target='GatsbyWindow'>Gatsby karkasu.</a> Vėliau prijungsiu Back-End technologijas.</div>
 			</div>
 		</section>
 	  </div>
