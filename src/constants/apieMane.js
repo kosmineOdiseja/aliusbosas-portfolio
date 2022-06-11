9c41faaf0acd0a590c9d437f7e82936b3c5ac1de
Alius <aliusbosas@gmail.com>
2022-06-11 16:41:49 +0300

    about section papildymas

--- src/constants/apieMane.js
+++ src/constants/apieMane.js
@@ -1,7 +1,7 @@
 const apieMane = [
 	 {
 	 	"title": "apie mane",
-	 	"description": "Esu linksmas, draugiškas, atsakingas, mėgstu iššūkius. Gerai jaučiuosi darbuodamasis komandoje, taip pat nejaučiu diskomfortodarbuojantis individualiai.",
+	 	"description": "Esu linksmas, draugiškas, atsakingas, mėgstu iššūkius. Gerai jaučiuosi darbuodamasis komandoje, taip pat nejaučiu diskomforto darbuojantis individualiai.",
 	 },
 	{
 	 	"title": "apie discipliną",
@@ -9,19 +9,19 @@ const apieMane = [
 	},
 	{
 	 	"title": " apie technologijų pasirinkimus",
-		"description": "Į programavimo srity ateinu su nuomone, kad nėra vieno geriausio karkaso ar vienos geriausios programavimo kalbos. Yra tik kliento problema, kuriai yra reikia įrankių, o tie įrankiai - programavimo kalbos, karkasai, bibliotekos, ar kita.",
+		"description": "Į programavimo sritį ateinu su nuomone, kad nėra vieno geriausio karkaso ar vienos geriausios programavimo kalbos. Yra tik kliento problema, kuriai išspręsti reikia įrankių, o tie įrankiai - programavimo kalbos, karkasai, bibliotekos, ar kita.",
 	},
 	{
 		 "title": "apie motyvaciją",
-	 	"description": "Labai mėgstu kantriai ir nuosekliai siekti savo tikslų.",
+	 	"description": "Labai mėgstu kantriai ir nuosekliai siekti savo tikslų. Sekmės istorijose didelį vaidmenį atlieka nuoširdus, nuoseklus darbas ir rutina.",
 	 },
 	{
 	 	"title": "apie tikslus",
-	 	"description": "Šiuo metu mano vienas pagrindinių tikslų: dirbi IT sferoje - Full-Stack programuotoju, bet noriu pradėti nuo Front-End, nes manau estetiška svetainė turi pranašumą, prieš svetainę turinčią didesnį funkcionalumą, bet prasčiau atrodančią. ",
+	 	"description": "Šiuo metu mano vienas pagrindinių tikslų: dirbi IT sferoje - Full-Stack programuotoju, bet noriu pradėti nuo Front-End, nes manau estetiška svetainė turi pranašumą, prieš svetainę turinčią didesnį funkcionalumą, bet prasčiau atrodančią. Full-stak'e galimybė sujungti tiek front-end grožį su back-end funkcionalumu atrodo labai įkvepiančiai.",
 	},
 	 {
 		"title": "apie darbo vietą",
-	 	"description": "Norėčiau darbuotis įmonėje, kurioje galėčiau kurti, mokytis, tobulėti ir augti netik kaip asmenybė, bet augti kartu su komanda.",
+	 	"description": "Norėčiau darbuotis įmonėje, kurioje galėčiau kurti, mokytis, tobulėti ir augti ne tik kaip asmenybė, bet augti kartu su komanda.",
 	},
 	{
 		 "title": "apie pomėgius",
