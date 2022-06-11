247527563aa8e8b6a8100b429ee8b1462fb5297e
Alius <aliusbosas@gmail.com>
2022-06-11 13:33:52 +0300

    skills section done

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -8,6 +8,10 @@ interface skillIknow {
 	name: string,
 	url: string,
 }
+interface skillIwant {
+	name: string,
+	url: string,
+}
 
 
 function Skills() {
@@ -15,39 +19,37 @@ function Skills() {
 	const [ learnMore, setLearnMore ] = useState(skillIwant)
 
   return (
-	<div>
-		<div className='bg-nightsky' >
-			<section className='m-auto w-9/12'>
-				<div className='flex flex-col gap-6 h-screen mx-auto p-6 items-center justify-center '>
-					<h2 className='text-6xl bg-green p-5 rounded-2xl text-nightsky '>Ką aš galiu naudoti projektuose?</h2>
-						<div className='flex flex-row gap-5 mx-auto p-6 items-start justify-center  bg-light-text rounded-2xl'>
+		<section className='bg-nightsky'>
+				<div className='h-full phone:h-screen tablet:w-9/12 mx-auto p-6 items-center justify-center flex flex-col gap-5 '>
+					<h2 className='bg-green text-nightsky mx-auto text-4xl rounded-lg font-semibold py-5 px-4 tablet:text-5xl text-center '>Ką aš galiu naudoti projektuose?</h2>
+						<div className='bg-light-text rounded-2xl mx-auto p-5 flex flex-row gap-5 flex-wrap'>
 							{skills.map((item: skillIknow, index: number) => {
 									return (
 										// <SkillCard key={index} item={item} />
-										<div className='bg-green rounded-2xl text-right' >
-											<h3 className='text-xl px-4 tablet:text-4xl' >{item.name}</h3>
-											<a href={item.url} className='text-green text-lg px-4 tablet:text-2xl' ></a>
+										<div className='bg-green rounded-2xl m-auto px-5 py-2.5'  > 
+											<a href={item.url} className='text-3xl' target="_blank">
+												<h3 className='text-3xl text-light-text text-center'>{item.name}</h3>
+											</a>
 										</div>
 									)
 							})}
-
 						</div>
-					<h2 className='text-6xl bg-green p-5 rounded-2xl text-nightsky '>Ką aš noriu išmokti?</h2>
-					<div className='flex flex-row gap-5 mx-auto p-6 items-start justify-center '>
+					<h2 className='bg-green text-nightsky mx-auto text-4xl rounded-lg font-semibold py-5 px-4 tablet:text-5xl text-center'>Ką aš noriu išmokti?</h2>
+					<div className='bg-light-text rounded-2xl mx-auto p-5 flex flex-row gap-5 flex-wrap'>
 						{learnMore.map((item: skillIwant, index: number) => {
 								return (
-									<div className='bg-green rounded-2xl text-right' key={index}>
-										<h3 className='text-xl px-4 tablet:text-4xll' >{item.name}</h3>
-										<a href={item.url} className='text-green text-lg px-4 tablet:text-2xl' > </a>
+									<div className='bg-green rounded-2xl px-5 py-2.5 m-auto' > 
+										<a href={item.url} className='text-3xl' target="_blank">
+											<h3 className='text-3xl text-light-text text-center'>{item.name}</h3>
+										</a>
 									</div>
 								)
 							})
 						}
 					</div>
+				{/* </div> */}
 				</div>
-			</section>
-		</div>
-	</div>
+	</section>
   )
 }
 
